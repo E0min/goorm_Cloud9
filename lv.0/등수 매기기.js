86d44b411f4e6a38f09ed2011aec7026e826da31
E0min <bbok4yo@gmail.com>
2024-09-04 16:34:22 +0900

    9/3

--- lv.0/등수 매기기.js
+++ lv.0/등수 매기기.js
@@ -1,22 +1,22 @@
 function solution(score) {
     var answer = new Array(score.length);
     let sumScore = [];
-    score.forEach(a=>{ // O(n)
-        sumScore.push(a[0]+a[1]);
-                  })
+    score.forEach(a => { // O(n)
+        sumScore.push(a[0] + a[1]);
+    })
 
-        for(let j = 0; j<sumScore.length;j++){ //O(n^2)
-            if(sumScore[j]==maxValue){
-                answer[j] = rank;
-                repeat++;
-                sumScore[j] = -1;
-            }
-            console.log(repeat)
+    for (let j = 0; j < sumScore.length; j++) { //O(n^2)
+        if (sumScore[j] == maxValue) {
+            answer[j] = rank;
+            repeat++;
+            sumScore[j] = -1;
         }
-        rank = rank + repeat;
-        
+        console.log(repeat)
     }
-    
-    
-    return answer;
+    rank = rank + repeat;
+
+
+
+
+return answer;
 }
