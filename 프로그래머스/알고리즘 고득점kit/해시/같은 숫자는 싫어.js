d922b853f618493b74891cace9aa438d64d32bf8
jyooni <berry3891@naver.com>
2024-10-01 15:57:00 +0900

    같은숫자는싫어

---
+++ 프로그래머스/알고리즘 고득점kit/해시/같은 숫자는 싫어.js
@@ -0,0 +1,18 @@
+function solution(arr){
+  // stack 사용
+  // 메모리: 83.3 MB, 시간: 45.73 ms / 정확성: 71.9 효율성: 28.1
+  const stack = [arr[0]];
+  const len = arr.length;
+  
+  for(let i = 1; i < len; i++){
+      if(arr[i] !== arr[i - 1]){
+          stack.push(arr[i]);
+      }
+  }
+  return stack;
+}
+
+// filter 사용
+// 메모리: 83 MB, 시간: 51.03 ms / 정확성: 71.9 효율성: 28.1
+// return arr.filter((val,index) => val !== arr[index+1]);
+
