7cad9ff6409189dbd7a2336d03f65e6f8d9cd031
clol7l_ <clol7l_@naver.com>
2024-10-01 15:12:34 +0900

    241001

---
+++ 프로그래머스/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{
+    let ans = [];
+    for(let i=0; i<arr.length ; i++){
+        if(arr[i] === arr[i+1]){
+            continue;
+        } else ans.push(arr[i]);
+    }
+    return ans;
+}
