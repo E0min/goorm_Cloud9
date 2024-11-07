9c86670df1bdb01fb5f3c9e103dd8b8124c9cdfa
jyooni <berry3891@naver.com>
2024-11-07 10:29:14 +0900

    merge

---
+++ 프로그래머스/알고리즘 고득점kit/완전탐색/모의고사.js
@@ -0,0 +1,25 @@
+function solution(answers) {
+  let [s1, s2, s3] = ["12345", "21232425", "3311224455"];
+  let students = { 1: 0, 2: 0, 3: 0 };
+  let answer = [];
+
+  answers.forEach((answer, idx) => {
+    answer = answer.toString();
+
+    if (s1[idx % s1.length] === answer) students[1]++;
+    if (s2[idx % s2.length] === answer) students[2]++;
+    if (s3[idx % s3.length] === answer) students[3]++;
+  });
+
+  //최대 점수 찾기
+  let max = Math.max(...Object.values(students));
+
+  // 최대 점수인 학생 찾기
+  for (let num in students) {
+    if (students[num] === max) {
+      answer.push(parseInt(num));
+    }
+  }
+
+  return answer;
+}
---
+++ 프로그래머스/알고리즘 고득점kit/탐욕법/체육복.js
@@ -0,0 +1,23 @@
+function solution(n, lost, reserve) {
+  // 여벌 체육복을 가져온 학생이 도난 당함 -> lost와 reserve에서 제거
+  let lostArr = lost.filter((student) => !reserve.includes(student));
+  let reserveArr = reserve.filter((student) => !lost.includes(student));
+
+  //정렬
+  lostArr = lostArr.sort((a, b) => a - b);
+  reserveArr = reserveArr.sort((a, b) => a - b);
+
+  // 도난 당한 학생에게 빌려주기
+  for (let i = 0; i < reserveArr.length; i++) {
+    let reserver = reserveArr[i];
+    let index = lostArr.findIndex(
+      (num) => num === reserver - 1 || num === reserver + 1
+    );
+
+    if (index !== -1) {
+      lostArr.splice(index, 1);
+    }
+  }
+
+  return n - lostArr.length;
+}
