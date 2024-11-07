067c14adbd9c964a19020b6900f8528163439ebc
jyooni <berry3891@naver.com>
2024-11-07 14:44:03 +0900

    이전 풀이들 추가

---
+++ 프로그래머스/알고리즘 고득점kit/스택_큐/올바른 괄호.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  let stack = [];
+
+  for (let char of s) {
+    if (char === "(") {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
